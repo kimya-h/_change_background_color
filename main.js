b6db622b9c82e5aa7ad1f4601515583d7db55eb6
kimya-h <kimya.mailbox@gmail.com>
2024-01-31 00:40:26 +0330

    adding function to change colors and finish

---
+++ main.js
@@ -0,0 +1,48 @@
+const colors = [
+    'crimson',
+    'deeppink',
+    'deepskyblue',
+    'gold',
+    'lightgrey',
+    'pink',
+    'orange',
+    'mediumturquoise',
+    'lemonchiffon',
+    'indigo',
+]
+
+function setColor(color) {
+    document.body.style.backgroundColor = color
+    document.querySelector('#current-color').innerHTML = color
+
+    const currenrActiveButton = document.querySelector('button.active')
+
+    if(currenrActiveButton !== null) {
+        currenrActiveButton.classList.remove('active')
+    }
+
+    document.querySelector(`button[data-color=${color}]`).classList.add('active')
+
+}
+
+colors.forEach(color => {
+    const button = document.createElement('button')
+    button.style.backgroundColor = color
+    button.setAttribute('data-color', color)
+
+    button.onclick = function () {
+        setColor(color)
+    }
+
+    document.querySelector('div.colors').appendChild(button)
+})
+
+document.body.onload = function () {
+    const randomIndex = Math.floor(Math.random() * colors.length)
+
+    const color = colors[randomIndex]
+    setColor(color)
+}
+
+
+
